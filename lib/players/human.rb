940c2aa53cedc090661822db291403a2f69441d5
Paul Fell-Cinoman <swellpf@gmail.com>
2018-12-14 20:40:00 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,11 @@ module Players
   class Human < Player
     
     def move(board)
+<<<<<<< HEAD
       puts "What is your move, human playing #{token}? (1-9)"
+=======
+      puts "What is your move, human? (1-9)"
+>>>>>>> 06914afb80bd93e4bbbb3a586257d8f0bb426a8b
       gets.strip
     end
   end
